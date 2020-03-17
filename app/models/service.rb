220ad5007dbf4c6457a0413be9a9f3444b833c6b
brian flanagan <brian@elbow.ie>
2020-03-17 14:32:43 +0000

    test scopes for services

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,36 @@
 class Service < ApplicationRecord
+  validates :hashed_id, presence: true, uniqueness: true
+  
+  scope :unpublished, -> { where(published_at: nil) }
+  scope :published, -> { where.not(published_at: nil) }
+  scope :for_category, -> (c) { c ? where('categories @> ?', "{#{ normalise_string(c) }}") : none }
+  scope :for_area, -> (a) { a ? where('areas @> ?', "{#{ normalise_string(a) }}") : none }
+
+  before_validation :normalise_areas, :normalise_categories
+
+  def to_param
+    hashed_id
+  end
+
+  def available_in?(area)
+    self.areas.include?(self.class.normalise_string(area))
+  end
+
+  def self.normalise_string(str)
+    str.downcase.strip.squeeze(' ')
+  end
+
+  private
+
+  def normalise_categories
+    self.categories = (self.categories || []).map { |s| self.class.normalise_string(s) }
+  end
+
+  def normalise_areas
+    self.areas = (self.areas || []).map { |s| self.class.normalise_string(s) }
+  end
+
+  def generate_hashed_id
+    self.hashed_id = Digest::SHA1.hexdigest([Time.now, rand].join).slice(0, 16)
+  end
 end
--- test/models/service_test.rb
+++ test/models/service_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ServiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is filterable by area" do
+    service = services(:valid)
+    service.areas << "Dublin 8"
+    service.areas << "Dublin 7"
+    service.save!
+
+    assert service.available_in?("Dublin 8")
+    assert_not service.available_in?("Dublin 6")
+
+    assert Service.for_area("Dublin 8").include?(service)
+    assert_not Service.for_area("Dublin 6").include?(service)
+  end
+
+  test "normalises arrays" do
+    service = services(:valid)
+    service.areas = nil
+    service.categories = nil
+    service.save!
+    
+    assert service.areas.is_a?(Array)
+    assert service.categories.is_a?(Array)
+    assert_equal service.areas.count, 0
+    assert_equal service.categories.count, 0
+  end
 end
