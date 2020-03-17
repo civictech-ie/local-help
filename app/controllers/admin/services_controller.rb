7ba0a5d2ebdb49257d61d1be62609bc060b40119
brian flanagan <brian@elbow.ie>
2020-03-17 20:05:18 +0000

    service CRUD

--- app/controllers/admin/services_controller.rb
+++ app/controllers/admin/services_controller.rb
@@ -8,14 +8,37 @@ class Admin::ServicesController < Admin::ApplicationController
   end
   
   def create
-
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to [:admin, :services]
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @service = Service.find_by(hashed_id: params[:id])
   end
 
   def update
+    @service = Service.find_by(hashed_id: params[:id])
+    if @service.update(service_params)
+      redirect_to [:admin, :services]
+    else
+      render :edit
+    end
+  end
 
+  def destroy
+    @service = Service.find_by(hashed_id: params[:id])
+    @service.destroy!
+    redirect_to [:admin, :services]
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:title, :description, :areas_str, :categories_str, :address_line_1, :address_line_2, :address_city, :address_county, :address_postcode, :contact_name, :contact_number, :contact_email, :contact_url)
   end
 end
+
--- app/controllers/api/services_controller.rb
+++ app/controllers/api/services_controller.rb
@@ -1,6 +1,12 @@
 class Api::ServicesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @services = Service.order('title desc')
     render json: @services
+  end
+
+  def create
+    raise params.inspect
   end
 end
